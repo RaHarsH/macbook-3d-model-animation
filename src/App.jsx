b58cdda3802b686e9d21e7978439216449f5eed6
Harshith R <harshith.raju0710@gmail.com>
2024-09-17 20:19:04 +0530

    added mac model

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import React, { useEffect } from 'react'
+import MacContainer from './components/MacContainer'
+import { Environment, ScrollControls } from '@react-three/drei'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Canvas camera={{ fov: 12, position: [0, -10, 220]}}>
+      <Environment 
+      files={[
+        'https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr'
+      ]}
+      />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls>
+    </Canvas>
   )
 }
 
--- src/components/MacContainer.jsx
+++ src/components/MacContainer.jsx
@@ -1,12 +1,36 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useScroll, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
 import React from 'react'
+import * as THREE from 'three'
 
 const MacContainer = () => {
-    const model = useGLTF('./models/mac.glb');
-  return (
-    <primitive >
+    let model = useGLTF('./models/mac.glb');
+    let texture = useTexture('./models/red.jpg')
+    let meshes = {}
 
-    </primitive>
+    model.scene.traverse(e => {
+        meshes[e.name] = e  // add all the meshes to the meshes object
+    })
+
+    // console.log(meshes)
+
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180); // to close the screen of the laptop initially
+
+    meshes.matte.material.map = texture;
+    meshes.matte.material.emissiveIntensity = 0;
+    meshes.matte.material.metalness = 0;
+    meshes.matte.material.roughness = 1;
+
+    let data = useScroll(); // gives value betweeen 0 and 1 based on the scroller position
+
+    useFrame((state, delta) => {
+        meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - (data.offset) * 90) // to move the screen (open or close)
+    })
+
+  return (
+    <group position={[0, -10, 20]}>
+        <primitive object={model.scene} />
+    </group>
   )
 }
 
