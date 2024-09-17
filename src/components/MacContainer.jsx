e9446c05a47a51cf702405fe431b853a3a285f35
Harshith R <harshith.raju0710@gmail.com>
2024-09-17 19:39:52 +0530

    initial stage

---
+++ src/components/MacContainer.jsx
@@ -0,0 +1,13 @@
+import { useGLTF } from '@react-three/drei'
+import React from 'react'
+
+const MacContainer = () => {
+    const model = useGLTF('./models/mac.glb');
+  return (
+    <primitive >
+
+    </primitive>
+  )
+}
+
+export default MacContainer
